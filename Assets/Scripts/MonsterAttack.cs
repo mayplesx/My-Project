2097dc521fd5512e94951e555f68b2afbadf5574
mayplesx <120215042+mayplesx@users.noreply.github.com>
2023-01-12 11:26:20 -0600

    map design and monster fixed! next steps are to do more map designs

---
+++ Assets/Scripts/MonsterAttack.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MonsterAttack : MonoBehaviour
+  {
+    PlayerHealth playerHealth;
+    [SerializeField]Animator animator;
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.tag == "Player")
+        {
+            animator.SetTrigger("Attack");
+            playerHealth = collider.gameObject.GetComponent<PlayerHealth>();
+            playerHealth.UpdateHealth(-8);
+        }
+    }
+}
